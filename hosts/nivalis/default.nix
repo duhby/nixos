89d0fa5178cdfc4091f9525e59efe286e50db12e
Josh <epikmemez@outlook.com>
2025-07-19 19:03:42 -0600

    chore: refactor default.nix

--- hosts/nivalis/default.nix
+++ hosts/nivalis/default.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, inputs, ... }:
 
 let
@@ -13,30 +9,24 @@ let
   weztermPkg = getPkg "wezterm";
 in {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # $ fwupdmgr update (firmware)
+  services.fwupd.enable = true;
+
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nivalis"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Denver";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -49,27 +39,24 @@ in {
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Configure keymap in X11
+  # Keyboard layout
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.josh = {
     isNormalUser = true;
-    description = "Josh";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    extraGroups = [ "networkmanager" "wheel" "audio" "video" ];
+    hashedPassword = "$6$oP3MZdVPRho1KWaD$1f8KQL5bUxyJOUJVqhlvtgVse250Q3uanCmx5AJw8/hcKorc99Q6rbgyYziclSWZwCEWpAmOUd8Ejk4FxN0mn1";
+    # There's only one user, so it's easier if everything is installed to systemPackages
+    #packages = with pkgs; [];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Environment
   environment.systemPackages = with pkgs; [
     neovim
     btop
@@ -86,7 +73,15 @@ in {
     du-dust
   ];
   environment.variables.EDITOR = "nvim";
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    open-sans
+  ];
+
+  # Shell
   programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
   programs.starship = {
     enable = true;
     presets =
@@ -94,66 +89,33 @@ in {
         "nerd-font-symbols"
       ];
   };
-  users.defaultUserShell = pkgs.zsh;
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.jetbrains-mono
-    open-sans
-  ];
-
-  services.fwupd.enable = true;
-
+  # Display manager
   services.greetd = {
     enable = true;
     settings.default_session = {
-      #command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-session --time --cmd ${swayfxPkg}/bin/sway";
+      #--sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions
       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --asterisks --remember --time --cmd ${swayfxPkg}/bin/sway";
       user = "greeter";
     };
   };
+  # currently broken
+  #services.displayManager.ly = {
+  #  enable = true;
+  #  settings = {
+  #    vi_mode = true;
+  #    animation = "gameoflife";
+  #    bigclock = "en";
+  #  };
+  #};
 
+  # Garbage collection
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
 
-  #services.displayManager.ly = {
-  #  enable = true;
-    # TODO: fix
-    #settings = {
-    #  vi_mode = true;
-    #  animation = "gameoflife";
-    #  bigclock = "en";
-    #};
-  #};
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  # https://nixos.org/nixos/options.html
+  system.stateVersion = "25.05";
 }
